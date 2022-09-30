79390b55ce0c5b35baf851764d41738a455a1b4c
Cuke7 <cuke.dev@mail.com>
2022-09-30 20:03:27 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="h-screen">
+      <div class="flex justify-center items-center">
+        Hello world!
+      </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
