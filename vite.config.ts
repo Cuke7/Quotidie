cda137ea77c329c34019db424705a1381dc6110b
Cuke7 <cuke.dev@mail.com>
2022-10-01 12:43:40 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VitePWA({
-    devOptions: {
-      enabled: true
-      /* other options */
-    },
+    // devOptions: {
+    //   enabled: true
+    //   /* other options */
+    // },
     registerType: 'autoUpdate', manifest: {
       name: "Quotidie",
       lang: "fr",
